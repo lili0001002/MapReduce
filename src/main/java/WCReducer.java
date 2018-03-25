debb92abb4bb013fb0cbe64165705adecedd5516
lili0001002 <178458471@qq.com>
2018-03-25 14:35:51 +0800

    初步入门

---
+++ src/main/java/WCReducer.java
@@ -0,0 +1,16 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WCReducer extends Reducer<Text,LongWritable,Text,LongWritable>{
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+        long counter = 0;
+        for(LongWritable i :values) {
+            counter += i.get();
+        }
+        context.write(key,new LongWritable(counter));
+    }
+}
---
+++ src/main/java/WordCount.java
@@ -0,0 +1,28 @@
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+    public static void main(String [] args) throws Exception{
+        Job job = Job.getInstance(new Configuration());
+       //设置Mapper的属性
+        job.setMapperClass(WCMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        FileInputFormat.setInputPaths(job,new Path());
+
+        job.setReducerClass(WCReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+        FileOutputFormat.setOutputPath(job,new Path());
+
+        job.waitForCompletion(true);
+
+        //打成jar包在hadoop上运行   具体操作  ：   hadoop jar /root/wc/jar
+    }
+}
